800fe7064207e452b6371d5f9339f4868a9fe32e
Rahman Sidd <rahman@stellium.com>
2024-12-31 13:37:50 +0000

    fix to initial app loading

--- podplugins/webapp/materialConsumptionPlugin/builder/PropertyEditor.js
+++ podplugins/webapp/materialConsumptionPlugin/builder/PropertyEditor.js
@@ -1,127 +1,100 @@
-sap.ui.define([
-    "sap/ui/model/json/JSONModel",
-    "sap/dm/dme/controller/BrowseBase",
-    "stellium/ext/podplugins/materialConsumptionPlugin/controller/ComponentBrowse",
-    "sap/dm/dme/formatter/StatusFormatter",
-    "sap/dm/dme/formatter/ObjectTypeFormatter"
-], function (JSONModel, BrowseBase, ComponentBrowse, StatusFormatter, ObjectTypeFormatter) {
-    "use strict";
+sap.ui.define([ 
+    "sap/ui/model/resource/ResourceModel", 
+    "sap/dm/dme/podfoundation/control/PropertyEditor" 
+], function(ResourceModel, PropertyEditor) {
+"use strict";
 
-    var BomBrowseType = BrowseBase.extend("stellium.ext.podplugins.materialConsumptionPlugin.controller.BomBrowse", {
+return PropertyEditor.extend("stellium.ext.podplugins.materialConsumptionPlugin.builder.PropertyEditor", {
+    constructor : function(sId, mSettings) {
+        PropertyEditor.apply(this, arguments);
+        this.setI18nKeyPrefix("materialConsumptionConfig.");
+        this.setResourceBundleName("stellium.ext.podplugins.materialConsumptionPlugin.i18n.builder");
+        this.setPluginResourceBundleName("stellium.ext.podplugins.materialConsumptionPlugin.i18n.i18n");
+    },
 
-        objectTypeFormatter: ObjectTypeFormatter,
-        statusFormatter: StatusFormatter,
+    addPropertyEditorContent : function(oPropertyFormContainer) {
+        var oData = this.getPropertyData();
+        
+        this.addSwitch(oPropertyFormContainer, "showAddButton", oData);
+        this.addSwitch(oPropertyFormContainer, "showScanButton", oData);
+        this.addSwitch(oPropertyFormContainer, "autoOpenScanPopup", oData);
+        this.addSwitch(oPropertyFormContainer, "allowFreeTextForBatch", oData);
+        this.addSwitch(oPropertyFormContainer, "showCoByProduct", oData);
+        this.addSwitch(oPropertyFormContainer, "showAlternateBomComponents", oData);
 
-        /**
-         * @override
-         */
-        populateSelectItems: function () {
-            this.getDialog().setModel(new JSONModel(StatusFormatter.getStatusList()), "statusItems");
-            this.getDialog().setModel(new JSONModel(ObjectTypeFormatter.getBomTypeList()), "bomTypeItems");
-        },
+        this.addInputField(oPropertyFormContainer, "charcForAdvSearch", oData);
+        this.addInputField(oPropertyFormContainer, "customField1", oData);
 
-        /**
-         * @override
-         */
-        createResultData: function (oBindingContext,oClickedListItem) {
-        	oBindingContext = oClickedListItem.getBindingContext("product");
-            return {
-                ref: oBindingContext.getProperty("ref"),
-                name: oBindingContext.getProperty("bom"),
-                version: oBindingContext.getProperty("version")
-            };
-        },
+        this._oActionButtonSelect = this.initializedActionButtons(oPropertyFormContainer, "selectActionButtonId", oData);
+        //For Weigh-Dispense
+        this.addSwitch(oPropertyFormContainer, "showWeighingSetZero", oData);
+        this.addInputField(oPropertyFormContainer, "charcSetZeroIndicator", oData);
+        this.addSwitch(oPropertyFormContainer, "showWeighingSetTare", oData);
+        this.addInputField(oPropertyFormContainer, "charcSetTareIndicator", oData);
+        this.addInputField(oPropertyFormContainer, "charcTareWeightIndicator", oData);
+        this.addInputField(oPropertyFormContainer, "charcWeighingIndicatorName", oData);
+        this.addInputField(oPropertyFormContainer, "charcUOMIndicatorName", oData);
+        // End of For Weigh-Dispense
+    },
 
-        /**
-         * @override
-         */
-        getExternalFilter: function () {
-            var sName = this.byId("componentFilter").getValue();
-            var sVersion = this.byId("componentVersionFilter").getValue();
-            return this._createFilterByComponent(sName, sVersion);
-        },
-
-        onComponentBrowse: function (oEvent) {
-            var sValue = oEvent.getSource().getValue();
-            ComponentBrowse.open(this.getDialog(), sValue, function (oSelectedObject) {
-                this.byId("componentFilter").setValue(oSelectedObject.name);
-                this.byId("componentVersionFilter").setValue(oSelectedObject.version);
-                this.onFilterBarChange();
-            }.bind(this));
-        },
-
-        _createFilterByComponent: function (sName, sVersion) {
-            var oFilterByName;
-            var oFilterByVersion;
-            var oConditionFilter;
-            var oFilter;
-
-            if (sName) {
-                oFilterByName = new sap.ui.model.Filter({
-                    path: "item/material/material",
-                    operator: sap.ui.model.FilterOperator.StartsWith,
-                    value1: sName
-                });
+    handleInputChange : function (sDataFieldName, sValue, oSource) {
+        var oData = this.getPropertyData();
+        if(sDataFieldName !== "customField1"){
+            var isValid = this.validateInputRegEx(sValue);
+            if(!isValid) {
+                oSource.setValueState(sap.ui.core.ValueState.Error);
+                oSource.setValueStateText(this.getI18nText("materialConsumptionConfig.invalidCharcName"));
             }
-
-            if (sVersion) {
-                oFilterByVersion = new sap.ui.model.Filter({
-                    path: "item/material/version",
-                    operator: sap.ui.model.FilterOperator.StartsWith,
-                    value1: sVersion
-                });
+            else if(sValue.split(",").length > 4) {
+                oSource.setValueState(sap.ui.core.ValueState.Error);
+                oSource.setValueStateText(this.getI18nText("materialConsumptionConfig.charcNameLimit"));
             }
-
-            if (sName && sVersion) {
-                oConditionFilter = new sap.ui.model.Filter([oFilterByName, oFilterByVersion], true);
-            } else if (sName) {
-                oConditionFilter = oFilterByName;
-            } else if (sVersion) {
-                oConditionFilter = oFilterByVersion;
+            else {
+                oData[sDataFieldName] = sValue;
+                oSource.setValueState(sap.ui.core.ValueState.None);
+                oSource.setValueStateText("");
             }
-
-            if (oConditionFilter) {
-                oFilter = new sap.ui.model.Filter({
-                    path: "components",
-                    operator: sap.ui.model.FilterOperator.Any,
-                    variable: "item",
-                    condition: oConditionFilter
-                });
-            }
-
-            return oFilter;
+        }else{
+            oData[sDataFieldName] = sValue;
         }
-    });
+    },
 
-    return {
-
-        /**
-         * Instantiates and opens value help dialog.
-         * @param {sap.ui.core.Element} oParentControl - value help dialog will be set as dependent to it.
-         * @param {String} sDefaultSearchValue - default value placed in a search field and a list is filtered by.
-         * @param fnSelectionCallback - callback function called when user selects item in a list.
-         */
-        open: function (oParentControl, sDefaultSearchValue, fnSelectionCallback, oModel) {
-            return new BomBrowseType("bomBrowse", {
-
-                oModel: oModel,
-                sFragmentName: "stellium.ext.podplugins.materialConsumptionPlugin.view.browse.BomBrowse",
-                oParentControl: oParentControl,
-                sDefaultSearchValue: sDefaultSearchValue,
-                fnSelectionCallback: fnSelectionCallback,
-                oFilterSettings: {
-                    aLiveSearchProperties: ["bom", "description"],
-                    sListBindingPath: "/Boms",
-                    aVariantFilterInfo: [
-                        { sFilterItemName: "bom" },
-                        { sFilterItemName: "description" },
-                        { sFilterItemName: "status" },
-                        { sFilterItemName: "bomType" },
-                        { sFilterItemName: "creationTimeRange", sSearchProperty: "createdDateTime" },
-                        { sFilterItemName: "currentVersion", oFilterOperator: sap.ui.model.FilterOperator.Any }
-                    ]
-                }
-            });
+    validateInputRegEx: function (sInputValue) {
+        //Regex for Valid Characters
+        var regex = /^[A-Za-z0-9_,]+$/;
+        var isValidInput = true;
+        if (sInputValue) {
+            if (!sInputValue.match(regex)) {
+                isValidInput = false;
+            }
         }
-    };
+        return isValidInput;
+    },
+
+    handleSelectChange : function (sDataFieldName, sSelectionValue) {
+        var oData = this.getPropertyData();
+        oData[sDataFieldName] = sSelectionValue;
+
+    },
+
+    handleSwitchChange: function (sDataName, bSelected) {
+        var oData = this.getPropertyData();
+        oData[sDataName] = bSelected;
+    },
+
+    getDefaultPropertyData : function() {
+        var oData = {
+        	"showAddButton" : true,
+        	"showScanButton" : true,
+            "autoOpenScanPopup" : false,
+            "allowFreeTextForBatch" : false,
+            "charcForAdvSearch" : "",
+            "showCoByProduct":false,
+            "customField1":"",
+            "selectActionButtonId":"",
+            "showAlternateBomComponents": false
+        };
+        return oData;
+    }
+});
 });
