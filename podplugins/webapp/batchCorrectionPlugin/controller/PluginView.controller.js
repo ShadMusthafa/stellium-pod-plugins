64d5607025dcea64e34c1af3b24f3ac363d2b77a
=Shad Musthafa <=shad.musthafa@stellium.com>
2025-01-22 14:00:58 +0530

    Created Batch Correction Plugin

---
+++ podplugins/webapp/batchCorrectionPlugin/controller/PluginView.controller.js
@@ -0,0 +1,215 @@
+sap.ui.define(
+  [
+    'sap/ui/model/json/JSONModel',
+    'sap/dm/dme/podfoundation/controller/PluginViewController',
+    'sap/base/Log',
+    '../utils/formatter',
+    'sap/m/MessageBox'
+  ],
+  function(JSONModel, PluginViewController, Log, formatter, MessageBox) {
+    'use strict';
+
+    var oLogger = Log.getLogger('batchCorrectionPlugin', Log.Level.INFO);
+
+    var oPluginViewController = PluginViewController.extend('stellium.ext.podplugins.batchCorrectionPlugin.controller.PluginView', {
+      metadata: {
+        properties: {}
+      },
+
+      oFormatter: formatter,
+
+      onInit: function() {
+        if (PluginViewController.prototype.onInit) {
+          PluginViewController.prototype.onInit.apply(this, arguments);
+        }
+
+        this.currentScaleFactor = 1;
+
+        var oView = this.getView();
+        var oViewData = {
+          scaleFactor: {
+            value: 1,
+            min: 1,
+            max: 15,
+            stepSize: 1,
+            precision: 0
+          }
+        };
+        oView.setModel(new JSONModel(oViewData), 'viewModel');
+        oView.setModel(new JSONModel([]), 'routingData');
+        oView.setModel(new JSONModel({}), 'giData');
+      },
+
+      /**
+     * @see PluginViewController.onBeforeRenderingPlugin()
+     */
+      onBeforeRenderingPlugin: function() {
+        this.subscribe('orderSelectionEvent', this.handleOrderSelectionEvent, this);
+        this.publish('requestForOrderData', { source: this, sendToAllPages: true });
+      },
+
+      onExit: function() {
+        if (PluginViewController.prototype.onExit) {
+          PluginViewController.prototype.onExit.apply(this, arguments);
+        }
+
+        this.unsubscribe('orderSelectionEvent', this.handleOrderSelectionEvent, this);
+      },
+
+      onBeforeRendering: function() {},
+
+      onAfterRendering: function() {},
+
+      handleOrderSelectionEvent: async function(sChannelId, sEventId, oData) {
+        console.log('Order selection event', oData);
+        var oView = this.getView();
+
+        this.selectedOrder = oData;
+
+        //Get routing information for selected order
+        var aRoutingData = await this._getRoutingDetailsForOrder(oData.order);
+        oView.getModel('routingData').setData(aRoutingData);
+        console.log('Routing Data ', aRoutingData);
+
+        //Get goods issue summary for the components
+        var aLineItems = await this._getGoodsIssueSummaryForOrder(oData.order);
+        aLineItems.forEach(oItem => {
+          //Update line item status
+          oItem.status = '';
+          //TODO: Tolerance checks
+          if (oItem.consumedQuantity.value) {
+            if (oItem.consumedQuantity.value < oItem.targetQuantity.value) {
+              oItem.status = 'PARKED';
+            } else if (oItem.consumedQuantity.value > oItem.targetQuantity.value) {
+              oItem.status = 'BATCH_CORRECTION';
+            }
+          }
+        });
+        oView.getModel('giData').setProperty('/lineItems', aLineItems);
+        console.log('GI Summary Data: ', aLineItems);
+        this._setScaleFactorEnabled(false);
+      },
+
+      onCalculateBatchCorrectionBtnPress: function(oEvent) {
+        var sUrl =
+          this.getPublicApiRestDataSourceUri() +
+          '/pe/api/v1/process/processDefinitions/start?key=REG_0ae51ff6-5158-477f-bdea-546e8f9bffa1&async=false';
+
+        //Get the batch correction item
+        var oGiModel = this.getView().getModel('giData'),
+          aLineItems = oGiModel.getProperty('/lineItems');
+
+        var oBatchCorrectionItem = aLineItems.find(oItem => oItem.status === 'BATCH_CORRECTION');
+        if (!oBatchCorrectionItem) {
+          MessageBox.information(this.getI18nText('noItemsForBatchCorrectionMsg'));
+          return;
+        }
+
+        var sComponent = oBatchCorrectionItem.materialId.material,
+          fConsumedQty = parseFloat(oBatchCorrectionItem.consumedQuantity.value),
+          sConsumedQtyUom = oBatchCorrectionItem.consumedQuantity.unitOfMeasure.uom;
+
+        var oRequestBody = {
+          plant: this.getPodController().getUserPlant(),
+          headerMaterial: this.selectedOrder.materialName,
+          component: sComponent,
+          correctionQuantity: fConsumedQty,
+          uom: sConsumedQtyUom
+        };
+
+        this.ajaxPostRequest(
+          sUrl,
+          oRequestBody,
+          function(oResponse) {
+            aLineItems.forEach(oLineItem => {
+              var sComponent = oLineItem.materialId.material;
+              var oItem = oResponse.response.find(oResItem => oResItem.componenet === sComponent);
+              console.assert(!!oItem, 'Item not found in response object');
+
+              oLineItem.batchCorrectionWeight = {
+                value: oItem.calcqty,
+                unitOfMeasure: {
+                  uom: oItem.cuom
+                }
+              };
+
+              oLineItem.issueWeight = {
+                value: oItem.calcqty - oLineItem.consumedQuantity.value,
+                unitOfMeasure: {
+                  uom: oItem.cuom
+                }
+              };
+            });
+            oGiModel.setProperty('/lineItems', aLineItems);
+            this._setScaleFactorEnabled(true);
+          }.bind(this)
+        );
+      },
+
+      onStepInputChange: function(oEvent) {
+        var oGiModel = this.getView().getModel('giData'),
+          aLineItems = oGiModel.getProperty('/lineItems'),
+          newValue = oEvent.getSource().getValue();
+
+        aLineItems.forEach(oItem => {
+          oItem.batchCorrectionWeight.value = oItem.batchCorrectionWeight.value / this.currentScaleFactor * newValue;
+          oItem.issueWeight.value = oItem.batchCorrectionWeight.value - oItem.consumedQuantity.value;
+        });
+
+        oGiModel.setProperty('/lineItems', aLineItems);
+        this.currentScaleFactor = newValue;
+      },
+
+      _getRoutingDetailsForOrder: function(sOrderId) {
+        var sUrl = this.getPublicApiRestDataSourceUri() + 'routing/v1/routings';
+        var oParams = {
+          plant: this.getPodController().getUserPlant(),
+          routing: sOrderId,
+          type: 'SHOPORDER_SPECIFIC_RECIPE'
+        };
+
+        return new Promise((resolve, reject) => {
+          this.ajaxGetRequest(sUrl, oParams, resolve, reject);
+        });
+      },
+
+      _getGoodsIssueSummaryForOrder: function(sOrderId) {
+        var oRoutingModel = this.getView().getModel('routingData'),
+          aRoutes = oRoutingModel.getProperty('/');
+
+        if (!aRoutes || aRoutes.length < 1) {
+          //TODO: throw error
+          return;
+        }
+
+        var aRoutingSteps = aRoutes[0].routingSteps;
+        var sUrl = this.getPodController().getAssemblyDataSourceUri() + 'order/goodsIssue/summary';
+        var aPromises = aRoutingSteps.map(oStep => {
+          var sOperationActivity = oStep.routingOperation.operationActivity.operationActivity;
+          var oParams = {
+            shopOrder: this.selectedOrder.order,
+            batchId: this.selectedOrder.sfc,
+            operationActivity: sOperationActivity,
+            stepId: oStep.stepId
+          };
+
+          //Raise get request for gi summary for phase and step
+          return new Promise((resolve, reject) => {
+            this.ajaxGetRequest(sUrl, oParams, resolve, reject);
+          });
+        });
+
+        return Promise.all(aPromises).then(function(aResponses) {
+          var aLineItems = aResponses.flatMap(oResponse => oResponse.lineItems).filter(oItem => oItem.componentType === 'N');
+          return aLineItems;
+        });
+      },
+
+      _setScaleFactorEnabled: function(bFlag) {
+        this.byId('idStepInput').setEnabled(bFlag);
+      }
+    });
+
+    return oPluginViewController;
+  }
+);
---
+++ podplugins/webapp/batchCorrectionPlugin/utils/formatter.js
@@ -0,0 +1,71 @@
+sap.ui.define(['sap/dm/dme/constants/DMCConstants', 'sap/dm/dme/formatter/NumberFormatter'], function(DMCConstants, DMENumberFormatter) {
+  return {
+    getValidTargetValues: function(totalQtyEntryUom, totalQtyBaseUom, targetQuantity) {
+      var validTargetQty = this.oFormatter.getValidQty(totalQtyEntryUom, totalQtyBaseUom, targetQuantity);
+      return validTargetQty.value;
+    },
+
+    getValidQty: function(totalQtyEntryUom, totalQtyBaseUom, targetQuantity) {
+      if (totalQtyEntryUom && totalQtyEntryUom.value) {
+        return totalQtyEntryUom;
+      } else if (totalQtyBaseUom && totalQtyBaseUom.value) {
+        return totalQtyBaseUom;
+      } else {
+        return targetQuantity;
+      }
+    },
+
+    getValidConsumedQty: function(consumedQuantity, consumedQtyEntryUom) {
+      if (consumedQtyEntryUom && ![null, undefined, ''].includes(consumedQtyEntryUom.value)) {
+        return consumedQtyEntryUom;
+      } else {
+        return consumedQuantity;
+      }
+    },
+
+    formatQtyWithDecimals: function(quantity, uom) {
+      var formattedQty;
+      // To display decimals for UoM PC
+      if (DMCConstants.uomEach.includes(uom) && (quantity && quantity % 1 != 0)) {
+        var NumInstance = NumberFormat.getFloatInstance(
+          {
+            decimals: 3
+          },
+          sap.ui.getCore().getConfiguration().getLocale()
+        );
+        formattedQty = NumInstance.format(quantity);
+      } else {
+        formattedQty = DMENumberFormatter.dmcLocaleQuantityFormatterDisplay(quantity ? quantity : 0, uom);
+      }
+      return formattedQty;
+    },
+
+    getActualValue: function(consumedQuantity, consumedQtyEntryUom) {
+      var consumedQuantity = this.oFormatter.getValidConsumedQty(consumedQuantity, consumedQtyEntryUom);
+      var actualValue = this.oFormatter.formatQtyWithDecimals(consumedQuantity.value, consumedQuantity.unitOfMeasure.uom);
+      return actualValue + ' ' + consumedQuantity.unitOfMeasure.uom;
+    },
+
+    stateFormatter: function(sStatus) {
+      switch (sStatus) {
+        case 'PARKED':
+          return 'Warning';
+        case 'BATCH_CORRECTION':
+          return 'Error';
+        default:
+          return 'None';
+      }
+    },
+
+    statusTextFormatter: function(sStatus) {
+      switch (sStatus) {
+        case 'PARKED':
+          return 'Parked';
+        case 'BATCH_CORRECTION':
+          return 'Batch Correction';
+        default:
+          return '';
+      }
+    }
+  };
+});
