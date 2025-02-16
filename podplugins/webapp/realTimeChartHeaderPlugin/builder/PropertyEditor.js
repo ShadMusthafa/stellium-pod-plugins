b2e77b007ce6b0f02ab973c753912d9e77317018
=Shad Musthafa <=shad.musthafa@stellium.com>
2025-02-16 20:37:12 +0530

    Added realTimeChartHeaderPlugin to the project

---
+++ podplugins/webapp/realTimeChartHeaderPlugin/builder/PropertyEditor.js
@@ -0,0 +1,44 @@
+sap.ui.define([
+    "sap/ui/model/resource/ResourceModel",
+    "sap/dm/dme/podfoundation/control/PropertyEditor"
+], function (ResourceModel, PropertyEditor) {
+    "use strict";
+    
+    var oFormContainer;
+
+    return PropertyEditor.extend( "stellium.ext.podplugins.realTimeChartPlugin.builder.PropertyEditor" ,{
+
+		constructor: function(sId, mSettings){
+			
+			PropertyEditor.apply(this, arguments);
+			
+			this.setI18nKeyPrefix("customComponentListConfig.");
+			this.setResourceBundleName("stellium.ext.podplugins.realTimeChartPlugin.i18n.builder");
+			this.setPluginResourceBundleName("stellium.ext.podplugins.realTimeChartPlugin.i18n.i18n");
+		},
+		
+		addPropertyEditorContent: function(oPropertyFormContainer){
+			var oData = this.getPropertyData();
+			
+			this.addSwitch(oPropertyFormContainer, "backButtonVisible", oData);
+			this.addSwitch(oPropertyFormContainer, "closeButtonVisible", oData);
+						
+			this.addInputField(oPropertyFormContainer, "title", oData);
+			this.addInputField(oPropertyFormContainer, "text", oData);
+
+            oFormContainer = oPropertyFormContainer;
+		},
+		
+		getDefaultPropertyData: function(){
+			return {
+				
+				"backButtonVisible": true,
+				"closeButtonVisible": true,
+                "title": "realTimeChartPlugin",
+				"text": "realTimeChartPlugin"
+                
+			};
+		}
+
+	});
+});
