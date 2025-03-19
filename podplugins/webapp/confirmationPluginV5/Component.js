557db1b18f5a3f3407c4593d604a5d08bf6a1b75
=Shad Musthafa <=shad.musthafa@stellium.com>
2025-03-19 12:53:49 +0530

    Dev: Addded portioning confirmation plugin

---
+++ podplugins/webapp/confirmationPluginV5/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/dm/dme/podfoundation/component/production/ProductionUIComponent"
+], function(ProductionUIComponent) {
+    "use strict";
+
+    /**
+     * 
+     */
+    var Component = ProductionUIComponent.extend("stellium.ext.podplugins.confirmationPluginV5.Component", {
+        metadata : {
+            manifest : "json"
+        }
+    });
+
+    return Component;
+});
